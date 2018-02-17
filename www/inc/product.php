e494573432f1ced45007819da2266bcf307467fc
EndPositive <jop-zitman@hotmail.com>
2018-02-17 01:30:27 +0100

    Update Listing Removal

--- www/inc/product.php
+++ www/inc/product.php
@@ -1,6 +1,7 @@
 <?php
 
 $name = $product[0];
+$id = base64_encode($name);
 $price = $product[1];
 $img = $product[2];
 $categ = $product[4];
@@ -10,6 +11,6 @@ $sec = $product[6];
 $cur = isset($product[7]) ? $product[7] : '';
 
 $date = date('Y/m/d H:i:s', $sec);
-$prodcutCardHtml = '<div><div class="image" style="background-image: url(data:image/gif;base64,' . $img . ')"></div><p class="category">Category</p><p class="title">' . $name . '</p><a class="remove" href="/API/func.php?removeproduct=' . $name . '">Remove Product</a><hr><img class="currency" src="/img/eth.png"><p class="price">' . $price . ' ' . $cur . '</p><button class="smaller black buybutton"><i class="material-icons">shopping_basket</i>Buy This</div>';
+$prodcutCardHtml = '<div><div class="image" style="background-image: url(data:image/gif;base64,' . $img . ')"></div><p class="category">Category</p><p class="title">' . $name . '</p><a class="remove" onclick="removeproduct(\'' . $id . '\')">Remove Product</a><hr><img class="currency" src="/img/eth.png"><p class="price">' . $price . ' ' . $cur . '</p><button class="smaller black buybutton"><i class="material-icons">shopping_basket</i>Buy This</div>';
 
 ?>
